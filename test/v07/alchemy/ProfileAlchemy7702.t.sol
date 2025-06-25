5b9ae4dcd56d08c9443ae90749bb86f75bb6221f
leekt <leekt216@gmail.com>
2025-06-26 00:48:03 +0900

    7702 results

---
+++ test/v07/alchemy/ProfileAlchemy7702.t.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.8.0;
+
+import {AAGasProfileBase07} from "src/TestBase07.sol";
+import {DeployArtifact, ArtifactsLib} from "src/artifacts/ArtifactsLib.sol";
+import {PackedUserOperation} from "account-abstraction/interfaces/PackedUserOperation.sol";
+import {ECDSA} from "solady/utils/ECDSA.sol";
+import "./AlchemyArtifacts.sol";
+
+contract ProfileAlchemy7702 is AAGasProfileBase07 {
+    address implementation;
+
+    function _initializeTest() internal override {
+        name = "Alchemy 7702";
+
+        implementation = ArtifactsLib.deploy(DeployArtifact({initCode: MODULAR_ACCOUNT_ALCHEMY_7702_INITCODE, addr: MODULAR_ACCOUNT_ALCHEMY_7702_ADDR}));
+
+        vm.etch(owner, abi.encodePacked(hex"ef0100", address(implementation)));
+    }
+
+    function _createAccount() internal override returns (address) {
+        return owner;
+    }
+
+    function _fillData(address _recipient, uint256 _amount, bytes memory _data)
+        internal
+        override
+        returns (bytes memory)
+    {
+        return
+            abi.encodeWithSignature("execute(address,uint256,bytes)", _recipient, _amount, _data);
+    }
+
+    function _getNonce(PackedUserOperation memory _op) internal override returns (uint256) {
+        return entryPoint.getNonce(address(account), 0);
+    }
+
+    function initializeData() internal returns (bytes memory) {
+        return hex"";
+    }
+
+    function _getAccount() internal override returns (address) {
+        return owner;
+    }
+
+    function _getInitCode() internal override returns (bytes memory) {
+        return hex"";
+    }
+
+    function _getCreationGasLimit() internal override returns (uint256) {
+        return 1000000;
+    }
+
+    function _getSignature(PackedUserOperation memory _op) internal override returns (bytes memory) {
+        bytes32 hash = entryPoint.getUserOpHash(_op);
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        (v, r, s) = vm.sign(key, ECDSA.toEthSignedMessageHash(hash));
+        return abi.encodePacked(r, s, v);
+    }
+}
---
+++ test/v07/nexus/ProfileNexus7702.t.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.8.0;
+
+import {AAGasProfileBase07} from "src/TestBase07.sol";
+import {DeployArtifact, ArtifactsLib} from "src/artifacts/ArtifactsLib.sol";
+import {PackedUserOperation} from "account-abstraction/interfaces/PackedUserOperation.sol";
+import "./NexusArtifacts.sol";
+
+contract ProfileNexus is AAGasProfileBase07 {
+    INexusFactory public nexusFactory;
+    address ecdsaValidator;
+
+    address bootstrap;
+    address nexus;
+
+    function _initializeTest() internal override {
+        name = "Nexus 7702";
+
+        ecdsaValidator = ArtifactsLib.deploy(DeployArtifact({initCode: K1_VALIDATOR_INITCODE, addr: K1_VALIDATOR_ADDR}));
+
+        bootstrap =
+            ArtifactsLib.deploy(DeployArtifact({initCode: NEXUS_BOOTSTRAP_INITCODE, addr: NEXUS_BOOTSTRAP_ADDR}));
+
+        nexus = ArtifactsLib.deploy(DeployArtifact({initCode: NEXUS_INITCODE, addr: NEXUS_ADDR}));
+
+        nexusFactory = INexusFactory(
+            ArtifactsLib.deploy(DeployArtifact({initCode: NEXUS_FACTORY_INITCODE, addr: NEXUS_FACTORY_ADDR}))
+        );
+        vm.etch(owner, abi.encodePacked(hex"ef0100", address(nexus)));
+    }
+
+    function _createAccount() internal override returns (address) {
+        return owner;
+    }
+
+    function _fillData(address _recipient, uint256 _amount, bytes memory _data)
+        internal
+        override
+        returns (bytes memory)
+    {
+        return abi.encodeWithSelector(INexus.execute.selector, bytes32(0), abi.encodePacked(_recipient, _amount, _data));
+    }
+
+    function _getNonce(PackedUserOperation memory _op) internal override returns (uint256) {
+        return entryPoint.getNonce(address(account), 0);
+    }
+
+    function initializeData() internal returns (bytes memory) {
+        return hex"";
+    }
+
+    function _getAccount() internal override returns (address) {
+        return owner;
+    }
+
+    function _getInitCode() internal override returns (bytes memory) {
+        return hex"";
+    }
+
+    function _getCreationGasLimit() internal override returns (uint256) {
+        return 1000000;
+    }
+
+    function _getSignature(PackedUserOperation memory _op) internal override returns (bytes memory) {
+        bytes32 hash = entryPoint.getUserOpHash(_op);
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        (v, r, s) = vm.sign(key, hash);
+        return abi.encodePacked(r, s, v);
+    }
+}
